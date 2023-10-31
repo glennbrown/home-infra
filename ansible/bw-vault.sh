3cc69cea5a147869b750d7da540bad2ab7de752d
Glenn Brown <glenn.brown@gmail.com>
2023-10-31 15:44:28 -0400

    Started my move from 1Password to Bitwarden

---
+++ ansible/bw-vault.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e 
+
+VAULT_ANSIBLE_NAME="Ansible Vault"
+
+bw get password "${VAULT_ANSIBLE_NAME}"
--- ansible/op-become.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-BECOME_PASSWORD="Ansible Become"
-
-op item get --format json "$BECOME_PASSWORD" | jq '.fields[] | select(.id=="password").value' | tr -d '"'
