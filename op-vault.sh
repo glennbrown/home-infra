48c7902e8fcf4815e5d8fd023c41f5910ad6739f
Glenn Brown <glenn.brown@gmail.com>
2025-10-20 21:32:43 -0400

    Some fixes and update related to ansible-core 2.19 changes

--- op-vault.sh
+++ op-vault.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
+# Retrieve Ansible Vault password from 1Password, using the 1Password CLI with a service account
+# https://developer.1password.com/docs/service-accounts/
 set -e
 
-VAULT_ANSIBLE_NAME="Ansible Vault"
+# Load 1Password Service Account Token
+source ~/.op-service-account
 
-op item get --format json "$VAULT_ANSIBLE_NAME" |jq '.fields[] | select(.id=="password").value' | tr -d '"'
+# Item and Vault names
+OP_VAULT="Ansible Secrets"
+OP_ITEM="Ansible Vault"
+
+op item get --vault "$OP_VAULT" --format json "$OP_ITEM" |jq '.fields[] | select(.id=="password").value' | tr -d '"'
